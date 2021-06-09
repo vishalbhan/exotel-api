51bb371104ba835ed7eeb37752dfb0b670b36541
Vishal Bhan <vishalbhan@Vishals-MacBook-Pro.local>
2021-06-09 19:17:27 +0530

    added lists response

---
+++ pages/api/lists.js
@@ -0,0 +1,286 @@
+export default function handler (req, res) {
+    res.status(200).json({
+        response: [
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "2e7c54f59721436e8974a7acacb35fa0",
+                    "date_created": "2021-04-26T20:39:18Z",
+                    "date_updated": "2021-04-27T02:09:17Z",
+                    "account_sid": "exotel8u3",
+                    "name": "test_csv_list1",
+                    "uri": "/v2/accounts/exotel8u3/lists/2e7c54f59721436e8974a7acacb35fa0",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "cc87e5a6cdcc47a7a98cb1e6662cc21c",
+                    "date_created": "2021-04-22T05:19:32Z",
+                    "date_updated": "2021-04-22T10:49:32Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list31",
+                    "uri": "/v2/accounts/exotel8u3/lists/cc87e5a6cdcc47a7a98cb1e6662cc21c",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "97acc03502034ea4a74ab36f6676441b",
+                    "date_created": "2021-04-22T05:14:41Z",
+                    "date_updated": "2021-04-22T10:44:40Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list30",
+                    "uri": "/v2/accounts/exotel8u3/lists/97acc03502034ea4a74ab36f6676441b",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "02160127c96a4b83b97407832df9a3e8",
+                    "date_created": "2021-04-21T16:17:16Z",
+                    "date_updated": "2021-04-21T21:47:16Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list29",
+                    "uri": "/v2/accounts/exotel8u3/lists/02160127c96a4b83b97407832df9a3e8",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "416988db6abb4081b2f70c143744c535",
+                    "date_created": "2021-04-21T16:14:03Z",
+                    "date_updated": "2021-04-21T21:44:02Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list28",
+                    "uri": "/v2/accounts/exotel8u3/lists/416988db6abb4081b2f70c143744c535",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "553e63e1d72a4d44940a362e68cf2772",
+                    "date_created": "2021-04-21T16:12:37Z",
+                    "date_updated": "2021-04-21T21:42:36Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list27",
+                    "uri": "/v2/accounts/exotel8u3/lists/553e63e1d72a4d44940a362e68cf2772",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "dbfc8fce751c4388a1f3e9ab67651ce2",
+                    "date_created": "2021-04-21T16:10:10Z",
+                    "date_updated": "2021-04-21T21:40:09Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list26",
+                    "uri": "/v2/accounts/exotel8u3/lists/dbfc8fce751c4388a1f3e9ab67651ce2",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "148824e28f2c4fbdaff57957b5f01760",
+                    "date_created": "2021-04-21T16:06:09Z",
+                    "date_updated": "2021-04-21T21:36:09Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list25",
+                    "uri": "/v2/accounts/exotel8u3/lists/148824e28f2c4fbdaff57957b5f01760",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "890f62cad6cc4d77842fb4a54c311fb0",
+                    "date_created": "2021-04-21T16:05:13Z",
+                    "date_updated": "2021-04-21T21:35:13Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list24",
+                    "uri": "/v2/accounts/exotel8u3/lists/890f62cad6cc4d77842fb4a54c311fb0",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "b59fb2ae8c2f41329893b201bdf89b3a",
+                    "date_created": "2021-04-21T15:49:26Z",
+                    "date_updated": "2021-04-21T21:19:26Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list23",
+                    "uri": "/v2/accounts/exotel8u3/lists/b59fb2ae8c2f41329893b201bdf89b3a",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "b45ed944a1264b0e95ca18b2eed9cf88",
+                    "date_created": "2021-04-21T14:30:39Z",
+                    "date_updated": "2021-04-21T20:00:39Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list22",
+                    "uri": "/v2/accounts/exotel8u3/lists/b45ed944a1264b0e95ca18b2eed9cf88",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "d77595ba4ccb493cba3fee5ea3a6b880",
+                    "date_created": "2021-04-21T13:54:32Z",
+                    "date_updated": "2021-04-21T19:24:32Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list21",
+                    "uri": "/v2/accounts/exotel8u3/lists/d77595ba4ccb493cba3fee5ea3a6b880",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "ea98cd7671924be1a6459aa7da8a1391",
+                    "date_created": "2021-04-21T12:37:57Z",
+                    "date_updated": "2021-04-21T18:07:56Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list20",
+                    "uri": "/v2/accounts/exotel8u3/lists/ea98cd7671924be1a6459aa7da8a1391",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "eccffbe6cbda40ce9e67e72f647bcf61",
+                    "date_created": "2021-04-21T12:18:38Z",
+                    "date_updated": "2021-04-21T17:48:37Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list19",
+                    "uri": "/v2/accounts/exotel8u3/lists/eccffbe6cbda40ce9e67e72f647bcf61",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "fdd5332c105e4d67a670e82aba728754",
+                    "date_created": "2021-04-21T04:47:53Z",
+                    "date_updated": "2021-04-21T10:17:52Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list18",
+                    "uri": "/v2/accounts/exotel8u3/lists/fdd5332c105e4d67a670e82aba728754",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "8a344b5c237a40fa94908a6818c3ba8d",
+                    "date_created": "2021-04-20T05:53:04Z",
+                    "date_updated": "2021-04-20T11:23:03Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list17",
+                    "uri": "/v2/accounts/exotel8u3/lists/8a344b5c237a40fa94908a6818c3ba8d",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "489e119cf25341cc8e4c3007dad40407",
+                    "date_created": "2021-04-20T04:28:28Z",
+                    "date_updated": "2021-04-20T09:58:27Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list16",
+                    "uri": "/v2/accounts/exotel8u3/lists/489e119cf25341cc8e4c3007dad40407",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "6ed2739d984f47c1bc10b93475eeaa08",
+                    "date_created": "2021-04-19T17:51:43Z",
+                    "date_updated": "2021-04-19T23:21:43Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list15",
+                    "uri": "/v2/accounts/exotel8u3/lists/6ed2739d984f47c1bc10b93475eeaa08",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "86d17a16723347609ab6e83767fde219",
+                    "date_created": "2021-04-19T16:15:03Z",
+                    "date_updated": "2021-04-19T21:45:02Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list14",
+                    "uri": "/v2/accounts/exotel8u3/lists/86d17a16723347609ab6e83767fde219",
+                    "tag": null
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "87cf50ef25b349619dc2c99116a28bfc",
+                    "date_created": "2021-04-19T07:06:06Z",
+                    "date_updated": "2021-04-19T12:36:05Z",
+                    "account_sid": "exotel8u3",
+                    "name": "csv_list13",
+                    "uri": "/v2/accounts/exotel8u3/lists/87cf50ef25b349619dc2c99116a28bfc",
+                    "tag": null
+                }
+            }
+        ]
+    })
+}
