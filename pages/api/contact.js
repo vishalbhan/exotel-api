5203f034d2f6d44215d9337e1908a759ccec8248
Vishal Bhan <vishalbhan@Vishals-MacBook-Pro.local>
2021-06-09 18:56:31 +0530

    added contacts response

---
+++ pages/api/contact.js
@@ -0,0 +1,129 @@
+export default function handler(req, res) {
+    // const { pid } = req.query
+    res.end({
+        "request_id": "5e320c2bdb8444c59d4d270715905cfc",
+        "method": "GET",
+        "http_code": 200,
+        "metadata": {
+            "count": 3,
+            "offset": 0,
+            "limit": 20,
+            "total": 3
+        },
+        "response": [
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "6b8ebe6428844d98be747ac28000fc29",
+                    "date_created": "2021-05-02T14:36:54Z",
+                    "date_updated": "2021-05-02T14:36:54Z",
+                    "account_sid": "exotel8u3",
+                    "number": "+919645122023",
+                    "first_name": null,
+                    "last_name": null,
+                    "company_name": null,
+                    "email": null,
+                    "tag": null,
+                    "custom_field": null,
+                    "uri": "/v2/accounts/exotel8u3/contacts/6b8ebe6428844d98be747ac28000fc29",
+                    "lists": [
+                        {
+                            "sid": "1d2d733000f8422393582deea8f588f4",
+                            "date_created": "2021-05-02T16:23:27Z",
+                            "date_updated": "2021-05-02T16:23:26Z",
+                            "account_sid": "exotel8u3",
+                            "name": "list3",
+                            "uri": "/v2/accounts/exotel8u3/lists/1d2d733000f8422393582deea8f588f4",
+                            "tag": null,
+                            "contact_count": null
+                        }
+                    ]
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "58254233e1a84219b9d25b932cf0548c",
+                    "date_created": "2021-05-02T14:36:42Z",
+                    "date_updated": "2021-05-02T14:36:42Z",
+                    "account_sid": "exotel8u3",
+                    "number": "+919645122020",
+                    "first_name": null,
+                    "last_name": null,
+                    "company_name": null,
+                    "email": null,
+                    "tag": null,
+                    "custom_field": null,
+                    "uri": "/v2/accounts/exotel8u3/contacts/58254233e1a84219b9d25b932cf0548c",
+                    "lists": [
+                        {
+                            "sid": "1d2d733000f8422393582deea8f588f4",
+                            "date_created": "2021-05-02T16:23:27Z",
+                            "date_updated": "2021-05-02T16:23:26Z",
+                            "account_sid": "exotel8u3",
+                            "name": "list3",
+                            "uri": "/v2/accounts/exotel8u3/lists/1d2d733000f8422393582deea8f588f4",
+                            "tag": null,
+                            "contact_count": null
+                        },
+                        {
+                            "sid": "d022ccd6e00e4324b32be9f81f9b2140",
+                            "date_created": "2021-05-02T16:23:27Z",
+                            "date_updated": "2021-05-02T16:23:26Z",
+                            "account_sid": "exotel8u3",
+                            "name": "list4",
+                            "uri": "/v2/accounts/exotel8u3/lists/d022ccd6e00e4324b32be9f81f9b2140",
+                            "tag": null,
+                            "contact_count": null
+                        }
+                    ]
+                }
+            },
+            {
+                "code": 200,
+                "error_data": null,
+                "status": "success",
+                "data": {
+                    "sid": "45f64a6de96e4d50b1e7ccc00d52cb08",
+                    "date_created": "2021-05-02T14:36:31Z",
+                    "date_updated": "2021-05-02T14:36:31Z",
+                    "account_sid": "exotel8u3",
+                    "number": "+919645122021",
+                    "first_name": null,
+                    "last_name": null,
+                    "company_name": null,
+                    "email": null,
+                    "tag": null,
+                    "custom_field": null,
+                    "uri": "/v2/accounts/exotel8u3/contacts/45f64a6de96e4d50b1e7ccc00d52cb08",
+                    "lists": [
+                        {
+                            "sid": "1d2d733000f8422393582deea8f588f4",
+                            "date_created": "2021-05-02T16:23:27Z",
+                            "date_updated": "2021-05-02T16:23:26Z",
+                            "account_sid": "exotel8u3",
+                            "name": "list3",
+                            "uri": "/v2/accounts/exotel8u3/lists/1d2d733000f8422393582deea8f588f4",
+                            "tag": null,
+                            "contact_count": null
+                        },
+                        {
+                            "sid": "d022ccd6e00e4324b32be9f81f9b2140",
+                            "date_created": "2021-05-02T16:23:27Z",
+                            "date_updated": "2021-05-02T16:23:26Z",
+                            "account_sid": "exotel8u3",
+                            "name": "list4",
+                            "uri": "/v2/accounts/exotel8u3/lists/d022ccd6e00e4324b32be9f81f9b2140",
+                            "tag": null,
+                            "contact_count": null
+                        }
+                    ]
+                }
+            }
+        ]
+    })
+  }
